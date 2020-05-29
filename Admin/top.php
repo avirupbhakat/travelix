57e9e153419afea3d1e064e9107792e1dcb93881
avirupbhakat <avirupbhakat@gmail.com>
2020-05-29 12:42:25 +0530

    initial commit

---
+++ Admin/top.php
@@ -0,0 +1,21 @@
+	 <div class="header-top">
+		 <!--container-->
+		  <div class="container">
+			 <div class="top-nav">
+			 			<div class="logo">
+						<a href="../admin/index.php"><img src="../images/logo_2.png" id="section-1" class="img-responsive" alt=""/>travelix</a>
+						</div>
+						<div class="menu">
+						<ul id="nav">
+							 <li><a href="../index.php" target="_blank">Preview Website</a></li>
+							 <li><a href="logout.php">Log Out</a></li>
+						     <div class="clearfix"></div>
+						 </ul>
+						 </div>
+			 </div>
+			  <div class="clearfix"> </div>
+			
+
+		 </div>
+		 <!--/container-->
+	 </div>
---
+++ Admin/topforlogin.php
@@ -0,0 +1,15 @@
+	 <div class="header-top">
+		 <!--container-->
+		  <div class="container">
+			 <div class="top-nav">
+						<div class="logo">
+						<a href="../index.php"><img src="../images/logo_2.png" id="section-1" class="img-responsive" alt=""/>travelix</a>
+						</div>
+						
+			 </div>
+			  <div class="clearfix"> </div>
+			
+
+		 </div>
+		 <!--/container-->
+	 </div>
